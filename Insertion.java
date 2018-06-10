4e9ad608a6eecd67535ba494a0171650458d5d27
ajinkyamalhotra <35980852+ajinkyamalhotra@users.noreply.github.com>
2018-06-10 13:06:13 -0700

    Create Insertion.java

---
+++ Insertion.java
@@ -0,0 +1,18 @@
+public class Insertion{
+   public static void InsertionSort(int[] data, int size) {
+   	//Write your code here
+      int temp = 0;
+      for(int i=1; i<size; ++i){
+         temp = data[i];
+         int j=0;
+         for(j=i-1; j >= 0; --j){
+            if(data[j] > temp) data[j+1] = data[j];
+            else 
+               break;
+         }
+         data[j+1] = temp;
+      }
+   	//System.out.println("InserionSort");
+   	
+   }
+}
