96b00af4e62ae754a4974ff16b1733178962e5ac
ajinkyamalhotra <35980852+ajinkyamalhotra@users.noreply.github.com>
2018-06-14 16:28:03 -0700

    Update Quicksort.java

--- Quicksort.java
+++ Quicksort.java
@@ -1,19 +1,28 @@
- public class Quicksort{
+public class Quicksort{
+   
    public static void QuickSort(int[] data, int lo, int hi, int depth) {
     
       if(lo>= hi) 
          return;
+      
       else {
-         if(depth > maxDepth) maxDepth = depth;
+         if(depth > maxDepth)
+            maxDepth = depth;
+         
          int q = partition(data, lo, hi);
+         
          QuickSort(data, lo, q-1, depth++);   
+       
          QuickSort(data, q+1, hi, depth++);
-            
+       
+       
       }
    
    }
    public static int partition(int[] data, int lo, int hi){
+      
       int index = -1;
+      
       Random rand = new Random();
 
       index = rand.nextInt(hi+1-lo) + lo;
@@ -25,21 +34,36 @@
       
       int i = lo-1;
       for(int j=lo; j<=hi-1; j++){
+         
          //if(data[j] <= pivot && i+1 != j){
          if(data[j] <= pivot){
+            
             swap(data, i+1, j);
             i++;
+         
          }
+         
          //else if(i+1 == j) i++;
+      
       }
       swap(data, i+1, hi);
+      
       return i+1;
+   
    }
+ 
    public static void swap(int[] data, int index1, int index2){
+      
       if(index1 != index2){
+         
          int temp = data[index1];
+         
          data[index1] = data[index2];
+         
          data[index2] = temp;
+      
       }
+   
    }
+
 }
