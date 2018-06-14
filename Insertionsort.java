b91452046467ddd80f16c25d997b7c5160022254
ajinkyamalhotra <35980852+ajinkyamalhotra@users.noreply.github.com>
2018-06-14 15:38:21 -0700

    Update Insertionsort.java

--- Insertionsort.java
+++ Insertionsort.java
@@ -1,18 +1,40 @@
 public class Insertionsort{
+   
+   //Given I pass in the array and array size to the method
    public static void InsertionSort(int[] data, int size) {
-   	//Write your code here
+      
       int temp = 0;
+     
+      //from 1 to size-1
       for(int i=1; i<size; ++i){
+         
          temp = data[i];
+         
          int j=0;
+         
+         //from i-1 down to 0
          for(j=i-1; j >= 0; --j){
-            if(data[j] > temp) data[j+1] = data[j];
+            
+            if(data[j] > temp)
+               data[j+1] = data[j];
+            
             else 
                break;
+         
          }
+         
          data[j+1] = temp;
-      }
-   	//System.out.println("InserionSort");
+      }   	
    	
    }
+
 }
+/* add analysis and running time
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
